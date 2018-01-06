ed6dd68ed70e621dc251979f9b2c46089fd51133
Mathieu Bouchonnet <mathieubouchonnet@gmail.com>
2018-01-06 19:14:49 +0100

    Removed unecessary files

--- ResourceManager.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  ResourceManager.cpp
-//  SFML Engine
-//
-//  Created by Mathieu Bouchonnet on 06/01/2018.
-//  Copyright © 2018 Mathieu Bouchonnet. All rights reserved.
-//
-
-#include "ResourceManager.hpp"
--- TextureManager.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  TextureManager.cpp
-//  SFML Engine
-//
-//  Created by Mathieu Bouchonnet on 06/01/2018.
-//  Copyright © 2018 Mathieu Bouchonnet. All rights reserved.
-//
-
-#include "TextureManager.hpp"
